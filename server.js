3c950aedad2af7c20b8a748fc256954633d66c2d
Vivek Joy <vivekjne@gmail.com>
2021-02-21 13:37:02 +0530

    added auth and users route,user model and token authentication apis

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const path = require("path");
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,15 +11,16 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use("/api/users", require("./routes/api/users"));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
